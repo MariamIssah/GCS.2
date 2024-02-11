88438f0f5b7163bf7b782109860d2048fd8dd054
Siphocha <siphochakhala@gmail.com>
2024-02-11 01:01:55 -0800

    correction

--- student-record-management/select-emails.sh
+++ student-record-management/select-emails.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-## Select just the emails of the students
+## Select just the emails of the students.
 awk -F ', ' '{print $1}' students-list_1023.txt > student-emails.txt
 echo "Emails selected and saved in student-emails.txt."
